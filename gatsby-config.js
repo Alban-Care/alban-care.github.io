4dd4c5dfae4c4613db92320cec3ceb1b644021b0
Alban Caré <alban.care@gmail.com>
2023-01-09 14:08:37 +0000

    Initial commit & gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,32 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/",
   siteMetadata: {
-    title: `alban-care.github.io`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Alban Caré - Développeur Web Full Stack - Open Full | Friendly Remote`,
+    siteUrl: `https://alban-care.github.io`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
