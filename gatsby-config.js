8382a9f39ee45c8157d5ecf06e7467ed0697b3e3
Alban Caré <alban.care@gmail.com>
2023-01-09 13:46:23 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `alban-care.github.io`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
